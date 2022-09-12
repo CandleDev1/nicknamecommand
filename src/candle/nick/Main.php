f8495dc3f1fb5cb8367fa9101a4ecf14bbbb78e7
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-09-12 14:06:53 -0300

    Fix

--- src/candle/nick/Main.php
+++ src/candle/nick/Main.php
@@ -1,33 +1,22 @@
 <?php
 
-namespace candle\mask;
+namespace candle\nick;
 
-use candle\mask\Commands\givemask;
-use candle\mask\Listener\CreeperMask;
-use candle\mask\Listener\DragonMask;
+use candle\nick\Commands\NickCommand;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use candle\mask\Listener\PlayerMask;
 use pocketmine\scheduler\ClosureTask;
 
 class Main extends PluginBase implements Listener
 {
     public function onEnable(): void
     {
-        $this->getServer()->getLogger()->info("Masks been enabled");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->registerMask();
         $this->registerCommands();
     }
 
-    public function registerMask()
-    {
-        $this->getScheduler()->scheduleRepeatingTask(new PlayerMask(), 20);
-        $this->getScheduler()->scheduleRepeatingTask(new CreeperMask(), 20);
-        $this->getScheduler()->scheduleRepeatingTask(new DragonMask(), 20);
-    }
     public function registerCommands()
     {
-        $this->getServer()->getCommandMap()->register('givemask', new givemask());
+        $this->getServer()->getCommandMap()->register('nick', new nick());
     }
 }
